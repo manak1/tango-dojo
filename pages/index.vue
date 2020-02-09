abf52afca0e16cf84b97133b7e01f1f0bec7b704
manaki <mana23happy@hotmail.co.jp>
2020-02-09 23:25:56 +0900

    :+1:testupdate

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,21 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        tango-dojo
-      </h1>
-      <h2 class="subtitle">
-        My lovely Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="c-test">
+    <h2 class="c-test__text">testing</h2>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  components: {},
+  methods: {
+    testing() {
+      console.log('testing')
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -42,6 +23,12 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.c-test {
+  &__text {
+    color: red;
+  }
 }
 
 .title {
